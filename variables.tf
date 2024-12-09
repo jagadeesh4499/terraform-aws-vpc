100468449ad4df2cd2f02e271d45851e12d38882
jagadeesh <jagadeeshakula123@gmail.com>
2024-12-09 19:57:13 +0530

    terraform-aws-vpc

--- variables.tf
+++ variables.tf
@@ -1,19 +1,67 @@
 variable "project_name" {
-    type = string
+  type = string
 }
 variable "environment" {
-    type = string
+  type = string
 }
 variable "vpc_cidr" {
-    default = "10.0.0.0/16"
-    type = string
+  default = "10.0.0.0/16"
+  type    = string
 }
 variable "enable_dns_hostnames" {
-    default = true
+  default = true
 }
 variable "common_tags" {
-    default = {}
+  default = {}
 }
 variable "vpc_tags" {
-    default = {}
+  default = {}
+}
+variable "igw_tags" {
+  default = {}
+}
+variable "public_subnet_cidrs" {
+  type = list(any)
+  validation {
+    condition     = length(var.public_subnet_cidrs) == 2
+    error_message = "Please provide 2 valid public subnet CIDR"
+  }
+}
+variable "private_subnet_cidrs" {
+  type = list(any)
+  validation {
+    condition     = length(var.private_subnet_cidrs) == 2
+    error_message = "Please provide 2 valid private subnet CIDR"
+  }
+}
+variable "database_subnet_cidrs" {
+  type = list(any)
+  validation {
+    condition     = length(var.database_subnet_cidrs) == 2
+    error_message = "Please provide 2 valid database subnet CIDR"
+  }
+}
+variable "public_subnet_tags" {
+  default = {}
+}
+variable "private_subnet_tags" {
+  default = {}
+}
+variable "database_subnet_tags" {
+  default = {}
+}
+variable "db_subnet_group_tags" {
+  default = {}
+}
+variable "ngw_tags" {
+  default = {}
+}
+variable "public_route_table_tags" {
+  default = {}
+}
+variable "private_route_table_tags" {
+  default = {}
+}
+variable "database_route_table_tags" {
+  default = {}
 }
