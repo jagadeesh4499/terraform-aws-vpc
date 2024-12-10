fa6144ecbe79b86a9d57167bcee116190ce38d6e
jagadeesh <jagadeeshakula123@gmail.com>
2024-12-10 11:14:55 +0530

    terraform-aws-vpc

--- outputs.tf
+++ outputs.tf
@@ -4,3 +4,9 @@ output "vpc_id" {
 output "az_info" {
   value = data.aws_availability_zones.available
 }
+output "default_vpc_info" {
+  value = data.aws_vpc.default
+}
+output "main_route_table_info" {
+  value = data.aws_route_table.main
+}
